aae1aa6612df5e03c5379ad031aa846a2d23017d
opvel <opvel.hero@gmail.com>
2024-06-28 01:19:00 +0900

    feat: Fetch and store repository data and issues with labels

--- src/github/fetchData.js
+++ src/github/fetchData.js
@@ -3,23 +3,34 @@ import fs from 'fs';
 import path from 'path';
 
 const octokit = new Octokit();
+const __dirname = new URL(import.meta.url).pathname;
+const targetRepository = { owner: 'octocat', repo: 'Hello-World' };
 
 async function fetchRepo() {
     try {
-
-        const response = await octokit.rest.repos.get({
-            owner: 'octocat',
-            repo: 'Hello-World',
-        });
-
-        //fsでsrc/assets/repo.jsonに書き込む
-        const json = JSON.stringify(response.data, null, 2);
-        const __dirname = new URL(import.meta.url).pathname
-        fs.writeFileSync(path.resolve(__dirname, '../../assets/repo.json'), json);
-
+        const response = await octokit.rest.repos.get(targetRepository);
+        writeFile('repo', response.data);
     } catch (error) {
         console.log(error);
     }
 }
 
+async function fetchIssuesWithLabels() {
+    try {
+        const labels = ['経済', '医療・防災', '教育・子育て', '行政', '民主主義', 'その他'];
+        const response = await octokit.rest.issues.listForRepo(targetRepository);
+        const filteredIssues = response.data.filter(issue =>
+            issue.labels.some(label => labels.includes(label.name))
+        );
+        writeFile('issues', filteredIssues);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const writeFile = (fileName, data) => {
+    fs.writeFileSync(path.resolve(__dirname, `../../assets/${fileName}.json`), JSON.stringify(data, null, 2));
+}
+
 fetchRepo();
+fetchIssuesWithLabels();

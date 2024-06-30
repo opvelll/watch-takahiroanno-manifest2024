8ca431f4c45a65d12963d309cff9654e7ffeb2d5
opvel <opvel.hero@gmail.com>
2024-06-30 23:58:38 +0900

    feat: Add ExternalLinkButton component for opening external links

--- src/pages/Issues.tsx
+++ src/pages/Issues.tsx
@@ -1,7 +1,8 @@
 
-import { Button } from 'antd';
+
 import issues from '../assets/issues.json';
 import IssueCard from '../components/IssueCard';
+import ExternalLinkButton from '../components/ExternalLinkButton';
 
 export default function Issues() {
 
@@ -9,12 +10,16 @@ export default function Issues() {
 
     return (
         <>
-            <div className=''>
+            <div className='bg-white rounded-8 mb-4 flex flex-row justify-center items-center space-x-4 py-2'>
                 <div>
-                    GitHubのIssueページは<Button type='link' href='https://github.com/takahiroanno2024/election2024/issues'>こちら</Button>
+                    <ExternalLinkButton
+                        url='https://github.com/takahiroanno2024/election2024/issues'
+                        text='GitHubのIssueページはこちら ' />
                 </div>
                 <div>
-                    新しいIssueの立て方は<Button type='link' href='https://github.com/takahiroanno2024/election2024/blob/main/docs/manual_issue.md#new_issue'>こちら</Button>
+                    <ExternalLinkButton
+                        url='https://github.com/takahiroanno2024/election2024/blob/main/docs/manual_issue.md#new_issue'
+                        text='新しいIssueの立て方はこちら' />
                 </div>
             </div>
             <div className='p-4 bg-white rounded-8'>

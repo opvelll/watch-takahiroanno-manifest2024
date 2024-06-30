2ceade607677bc16d802c4cc0ca52f710a169063
opvel <opvel.hero@gmail.com>
2024-06-30 23:56:33 +0900

    feat: Add formatDate function for formatting date strings

--- src/components/IssueCard.tsx
+++ src/components/IssueCard.tsx
@@ -1,21 +1,11 @@
 import Markdown from "react-markdown";
 import { BiComment } from "react-icons/bi";
+import { formatDate } from "../lib/formatData";
 
 export default function IssueCard({ issue }: { issue: { title: string, body: string, createdAt: string, url: string, comments: { totalCount: number }, reactions: { totalCount: number } } }) {
     const openInNewTab = (url: string) => {
         window.open(url, '_blank', 'noopener,noreferrer');
     };
-    const formatDate = (dateString: string) => {
-        const date = new Date(dateString);
-        return new Intl.DateTimeFormat('ja-JP', {
-            year: 'numeric',
-            month: '2-digit',
-            day: '2-digit',
-            hour: '2-digit',
-            minute: '2-digit',
-        }).format(date);
-    };
-
     return (
         <div className="cursor-pointer hover:text-slate-500 p-2" onClick={() => openInNewTab(issue.url)}>
             <div className="rounded-8 border hover:border-slate-700 shadow bg-white">

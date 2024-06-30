e9f2a26a30a24ce82a4060065c0d4c66958c265a
opvel <opvel.hero@gmail.com>
2024-06-30 16:49:10 +0900

    feat: Fixed to be able to take more than 100 issues.

--- script/issuesFetcher.ts
+++ script/issuesFetcher.ts
@@ -1,48 +1,65 @@
 import { octokit, targetRepository } from "./setting.js";
 import { writeFile } from "./writeFile.js";
 
+const labels = [
+  "経済",
+  "医療・防災",
+  "教育・子育て",
+  "行政",
+  "民主主義",
+  "その他",
+];
+
 export async function fetchIssuesWithLabels(): Promise<void> {
   try {
-    const labels = [
-      "経済",
-      "医療・防災",
-      "教育・子育て",
-      "行政",
-      "民主主義",
-      "その他",
-    ];
+    let hasNextPage = true;
+    let endCursor = null;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const response: any = await octokit.graphql(
-      `
-  query($owner: String!, $repo: String!, $labels: [String!]) {
-    repository(owner:$owner, name: $repo) {
-      issues(labels: $labels) {
-        totalCount
-        nodes {
-          title
-          createdAt
-          url
-          state
-          body
-          comments {
-            totalCount
-          }
-          reactions(content: THUMBS_UP) {
-            totalCount
-          }
-          labels(first: 5) {
-            nodes {
-              name
+    let allIssues: any[] = [];
+
+    while (hasNextPage) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const response: any = await octokit.graphql(
+        `
+          query($owner: String!, $repo: String!, $labels: [String!], $after: String) {
+            repository(owner:$owner, name: $repo) {
+              issues(first:100, after: $after, labels: $labels, states:[OPEN,CLOSED]) {
+                totalCount
+                pageInfo {
+                  hasNextPage
+                  endCursor
+                }
+                nodes {
+                  title
+                  createdAt
+                  url
+                  state
+                  body
+                  comments {
+                    totalCount
+                  }
+                  reactions(content: THUMBS_UP) {
+                    totalCount
+                  }
+                  labels(first: 5) {
+                    nodes {
+                      name
+                    }
+                  }
+                }
+              }
             }
           }
-        }
-      }
-    }
-  }
         `,
-      { ...targetRepository, labels }
-    );
-    writeFile("issues", response.repository.issues.nodes);
+        { ...targetRepository, labels, after: endCursor }
+      );
+
+      allIssues = allIssues.concat(response.repository.issues.nodes);
+      hasNextPage = response.repository.issues.pageInfo.hasNextPage;
+      endCursor = response.repository.issues.pageInfo.endCursor;
+    }
+
+    writeFile("issues", allIssues);
   } catch (error) {
     console.error(error);
   }

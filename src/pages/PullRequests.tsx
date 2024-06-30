a2c1bcfe73b2f7a96322c1a5b3982efc79cb0b3a
opvel <opvel.hero@gmail.com>
2024-06-30 23:57:44 +0900

    feat: Add button for proposing changes on PullRequests page

--- src/pages/PullRequests.tsx
+++ src/pages/PullRequests.tsx
@@ -1,7 +1,20 @@
+import { Button } from "antd";
+
 export default function PullRequests() {
     return (
-        <div>
-            pullRequests
-        </div>
+        <>
+            <div className='bg-white rounded-8 mb-4 flex flex-row justify-center items-center space-x-4 py-2'>
+                <Button
+                    type='primary'
+                    shape="round"
+                    href='https://github.com/takahiroanno2024/election2024/blob/main/docs/manual_pull_request.md'>
+                    変更提案の仕方はこちら
+                </Button>
+            </div>
+            <div>
+                工事中
+            </div>
+        </>
+
     )
 }

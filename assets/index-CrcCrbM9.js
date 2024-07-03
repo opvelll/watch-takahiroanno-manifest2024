a65da7681ca0854e2fb81f4404e7ce030ee73fb0
opvelll <opvelll@users.noreply.github.com>
2024-07-03 23:33:07 +0000

    deploy: a0c2ff2341ef3d10003ca0738e70fe84489753f7

--- assets/index-CrcCrbM9.js
+++ assets/index-CrcCrbM9.js
@@ -9997,7 +9997,7 @@ https://www.moricli.jp/topics/2024/05/29/1385/\r
 \r
 ### 自由記述欄\r
 \r
-_No response_`,comments:{totalCount:4},reactions:{totalCount:6},labels:{nodes:[{name:"教育・子育て"},{name:"医療・防災"}]}},{title:"東京都の島嶼部航路を運航する船会社の都有化について",createdAt:"2024-07-03T11:03:12Z",url:"https://github.com/takahiroanno2024/election2024/issues/284",state:"OPEN",body:`### 政策ビジョン
+_No response_`,comments:{totalCount:5},reactions:{totalCount:6},labels:{nodes:[{name:"教育・子育て"},{name:"医療・防災"}]}},{title:"東京都の島嶼部航路を運航する船会社の都有化について",createdAt:"2024-07-03T11:03:12Z",url:"https://github.com/takahiroanno2024/election2024/issues/284",state:"OPEN",body:`### 政策ビジョン
 
 その他
 

9d2e346f30180aaea845f6d9aa55f463575031cb
opvel <opvel.hero@gmail.com>
2024-07-05 02:38:30 +0900

    feat: optimize CommitCard component and add associatedPullRequests

--- src/components/CommitCard.tsx
+++ src/components/CommitCard.tsx
@@ -1,29 +1,75 @@
 
 import DiffViewer, { DiffMethod } from 'react-diff-viewer';
 import { formatDate } from '../lib/formatData';
+import { Fragment } from 'react/jsx-runtime';
+// import issues from '../assets/issues.json';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function CommitCard({ commit }: { commit: any }) {
     const openInNewTab = (url: string) => {
         window.open(url, '_blank', 'noopener,noreferrer');
     };
 
+    // const getIssues = (commitMessage: string) => {
+    //     const issueNumbers = commitMessage.match(/#[0-9]+/g);
+    //     if (issueNumbers) {
+    //         return issueNumbers.map((issueNumber) => {
+    //             const issue = issues.find((issue: { number: number }) => issue.number === parseInt(issueNumber.replace('#', '')));
+    //             return issue;
+    //         });
+    //     }
+    //     return [];
+    // }
+
     return (
-        <div className="cursor-pointer border border-white  hover:shadow hover:border-slate-300 rounded-8 px-2 pb-2" onClick={() => openInNewTab(commit.url)}>
+        <div className="rounded-8 px-2 pb-2 border border-white  hover:shadow hover:border-slate-300">
             <div>
-                <div className='text-mono-14N-5 text-slate-600'>
-                    {formatDate(commit.committedDate)}
+                <div className='' onClick={() => openInNewTab(commit.url)}>
+                    <div className='text-mono-14N-5 text-slate-500'>
+                        {formatDate(commit.committedDate)}
+                    </div>
+                    <div className="text-std-16N-7 py-1 hover:text-std-16M-7 hover:cursor-pointer">
+                        {commit.message}
+                    </div>
                 </div>
-                <div className="text-std-16M-7">
-                    {commit.message}
-                </div>
+                {commit.associatedPullRequests.edges &&
+                    <div className="flex flex-col">
+                        <div className='text-dns-14B-3 pb-1'>■ 元になった変更提案(Pull Request)</div>
+                        <div className='text-dns-14N-3 ml-4'>
+                            {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+                            {commit.associatedPullRequests.edges.map((pr: { node: any }, idx: number) => (
+                                <Fragment key={idx}>
+                                    <div>
+                                        <a href={pr.node.url} target='_blank' rel='noreferrer noopener' className='text-primary underline'>{pr.node.title}</a>
+                                    </div>
+                                    <div>
+                                        {pr.node.timelineItems.nodes.map((node: { subject: { title: string, url: string } }, idx: number) => (
+                                            <div key={idx} className='ml-4'>
+                                                {node.subject &&
+                                                    <div className="flex flex-col pt-2">
+                                                        <div className='text-dns-14B-3 pb-1'>■ 元になった課題提起(Issue)</div>
+                                                        <div className='text-dns-14N-3 pl-4'>
+                                                            <a href={node.subject.url} target='_blank' rel='noreferrer noopener' className='text-primary underline'>{node.subject.title}
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                }
+                                            </div>
+                                        ))}
+                                    </div>
+                                </Fragment>
+                            ))}
+                        </div>
+                    </div>
+                }
             </div>
             <div className='space-y-2 pt-2'>
                 {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
                 {commit.files.map((file: any) => (
-                    <div key={file.sha} className='border rounded-8'>
+                    <div key={file.sha} className='border cursor-pointer hover:border-primary' onClick={() => openInNewTab(commit.url)}>
                         <div className='text-mono-14N-5 text-slate-600 px-2 py-1'>
                             {file.filename}
                         </div>
+
                         <div>
                             {file.patch && <DiffViewer
                                 oldValue={file.patch.split('\n').filter((line: string) => line.startsWith('-')).join('\n')}

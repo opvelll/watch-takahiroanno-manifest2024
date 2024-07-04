b419daf3db4450ffe74df2b12dc9f817546cc0c4
opvel <opvel.hero@gmail.com>
2024-07-04 20:28:46 +0900

    chore: Update tailwind.config.js to extend colors with primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1cc95f"
+      }
+    },
   },
   // eslint-disable-next-line no-undef
   plugins: [require('@digital-go-jp/tailwind-theme-plugin')],

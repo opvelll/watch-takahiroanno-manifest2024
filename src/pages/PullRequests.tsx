59a11f7968c3320d1f0ba935146be0a270c3bab7
opvel <opvel.hero@gmail.com>
2024-07-05 17:10:44 +0900

    feat: Add PullRequests page and component

--- src/pages/PullRequests.tsx
+++ src/pages/PullRequests.tsx
@@ -1,18 +1,34 @@
-import { Button } from "antd";
+
+import ExternalLinkButton from "../components/ExternalLinkButton";
+import pullRequests from "../assets/pullRequests.json";
+import { Fragment } from "react/jsx-runtime";
+import PullRequestCard from "../components/PullRequestCard";
+import ExternalLink from "../components/ExternalLink";
 
 export default function PullRequests() {
+    const sortedPullRequests = pullRequests.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
     return (
         <>
             <div className='bg-white rounded-8 mb-4 flex flex-row justify-center items-center space-x-4 py-2'>
-                <Button
-                    type='primary'
-                    shape="round"
-                    href='https://github.com/takahiroanno2024/election2024/blob/main/docs/manual_pull_request.md'>
-                    変更提案の仕方はこちら
-                </Button>
+                <ExternalLinkButton
+                    url='https://github.com/takahiroanno2024/election2024/blob/main/docs/manual_pull_request.md'
+                    text='変更提案(Pull Request)の仕方はこちら' />
             </div>
-            <div>
-                工事中
+            <div className='p-4 bg-white rounded-8'>
+                <div className='text-std-20B-5 pl-2 pt-2 pr-2 pb-4'>
+                    <ExternalLink href="https://github.com/takahiroanno2024/election2024/pulls?q=state%3Aopen%2Cclosed%2Cmerged+type%3Apr+">
+                        変更提案(PullRequests)の一覧
+                    </ExternalLink>
+                </div>
+
+                <div className="space-y-4">
+                    {/*  eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+                    {sortedPullRequests.map((pullRequest: any, idx: number) => (
+                        <Fragment key={idx}>
+                            <PullRequestCard pullRequest={pullRequest} />
+                        </Fragment>
+                    ))}
+                </div>
             </div>
         </>
 

e33d1e843172349d565f476d4af4c9d163522c63
opvelll <opvel.hero@gmail.com>
2024-11-03 17:56:18 +0900

    feat: improve Home component layout and add dividers

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Anchor } from "antd";
+import { Anchor, Divider } from "antd";
 import issues from "../assets/issues.json"
 import ExternalLinkButton from "../components/ExternalLinkButton";
 import IssueCard from "../components/IssueCard";
@@ -12,28 +12,19 @@ export default function Home() {
     //コメントがついているIssue5件
     const issuesWithMostComments = issues.sort((a, b) => b.comments.totalCount - a.comments.totalCount).slice(0, 5);
     return (
-
-        <div className="flex flex-col md:flex-row max-w-max">
-            <div className="hidden md:inline-block mr-10">
-                <Anchor items={[{ key: "about", href: "#about", title: "このサイトについて" },
-                { key: "latest", href: "#latestIssues", title: "最新のIssue" },
-                { key: "mostReaction", href: "#mostReactionIssues", title: "👍️の多いIssue" },
-                { key: "mostComments", href: "#mostCommentsIssues", title: "コメントの多いIssue" },
-
-                ]} />
-            </div>
-            <div className="space-y-6">
-                <div className="bg-white rounded-8  space-y-8 p-6  flex flex-col items-center">
-                    <div className="text-std-28B-5 md:text-std-32B-5">
-                        安野たかひろのマニフェストGitHubを眺めるサイト
+        <div className="flex flex-col">
+            <div className="space-y-6 md:px-28 lg:px-44 mb-10">
+                <div className=" rounded-8  space-y-8 p-6  flex flex-col items-center">
+                    <div className="text-std-28B-5 md:text-std-32B-5 lg:text-std-45B-4">
+                        Watch-TakahiroAnnno-Manifest
                     </div>
-                    <div>
+                    <div className="text-center lg:max-w-3xl ">
                         <div className="text-std-16N-7">
-                            東京都知事選挙の立候補者である安野たかひろ氏は、GitHub上でマニフェストを<a href="https://github.com/takahiroanno2024/election2024" target="_blank" rel="noopener noreferrer" className="text-primary underline">公開</a>し、
+                            東京都知事選挙の立候補者である安野たかひろ氏は、<a href="https://github.com/takahiroanno2024/election2024" target="_blank" rel="noopener noreferrer" className="text-primary underline">GitHub上でマニフェストを公開</a>し、
                             一般からの意見や提案を募るという取り組みを行っています。
                         </div>
                         <div className="text-std-16N-7">
-                            このサイトでは、安野氏のGitHubからマニフェストに関わるIssueと実際に取り込まれたコミットをAPIで取得し表示します。
+                            このサイトは、それを見やすくする試みです。
                         </div>
                         <div className="text-std-16N-7">
                             <span className="line-through">情報は30分ごとに更新されます。</span>（選挙活動期間が終わり、マニフェストレポジトリがアーカイブされたため、更新は行われません。）
@@ -55,52 +46,70 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="rounded-8 bg-white">
-                    <div className="text-std-20B-5 p-4" id="latestIssues">
-                        <a
-                            href="https://github.com/takahiroanno2024/election2024/issues?q=is%3Aopen+label%3A%E7%B5%8C%E6%B8%88%2C%E5%8C%BB%E7%99%82%E3%83%BB%E9%98%B2%E7%81%BD%2C%E6%95%99%E8%82%B2%E3%83%BB%E5%AD%90%E8%82%B2%E3%81%A6%2C%E8%A1%8C%E6%94%BF%2C%E6%B0%91%E4%B8%BB%E4%B8%BB%E7%BE%A9%2C%E3%81%9D%E3%81%AE%E4%BB%96" target="_blank" rel="noopener noreferrer"
-                            className="text-primary underline">最新のIssue</a>
+            </div>
+            <Divider />
+            <div className="flex flex-col md:flex-row max-w-max">
+                <div className="space-y-6">
+                    <div>
+                        <div className="text-std-20B-5 pb-4 text-center" id="mostReactionIssues">
+                            <a href="https://github.com/takahiroanno2024/election2024/issues?q=is%3Aissue+label%3A%E7%B5%8C%E6%B8%88%2C%E5%8C%BB%E7%99%82%E3%83%BB%E9%98%B2%E7%81%BD%2C%E6%95%99%E8%82%B2%E3%83%BB%E5%AD%90%E8%82%B2%E3%81%A6%2C%E8%A1%8C%E6%94%BF%2C%E6%B0%91%E4%B8%BB%E4%B8%BB%E7%BE%A9%2C%E3%81%9D%E3%81%AE%E4%BB%96+sort%3Areactions-%2B1-desc+"
+                                target="_blank" rel="noopener noreferrer"
+                                className="text-primary underline">
+                                👍️の多いIssue
+                            </a>
+                        </div>
+                        <div className="space-y-2">
+                            {issuesWithMostReactions.map((issue, idx) => (
+                                <Fragment key={idx}>
+                                    <IssueCard key={idx} issue={issue} />
+                                </Fragment>
+                            ))}
+                        </div>
                     </div>
-                    <div className="space-y-0 md:space-y-2">
-                        {latestIssues.map((issue, idx) => (
-                            <Fragment key={idx}>
-                                <IssueCard key={idx} issue={issue} />
-                            </Fragment>
-                        ))}
+                    <Divider />
+                    <div className="rounded-8">
+                        <div className="text-std-20B-5 pb-4 text-center" id="mostCommentsIssues">
+                            <a href="https://github.com/takahiroanno2024/election2024/issues?q=is%3Aissue+label%3A%E7%B5%8C%E6%B8%88%2C%E5%8C%BB%E7%99%82%E3%83%BB%E9%98%B2%E7%81%BD%2C%E6%95%99%E8%82%B2%E3%83%BB%E5%AD%90%E8%82%B2%E3%81%A6%2C%E8%A1%8C%E6%94%BF%2C%E6%B0%91%E4%B8%BB%E4%B8%BB%E7%BE%A9%2C%E3%81%9D%E3%81%AE%E4%BB%96+sort%3Acomments-desc"
+                                target="_blank" rel="noopener noreferrer"
+                                className="text-primary underline">
+                                コメントの多いIssue
+                            </a>
+                        </div>
+                        <div className="space-y-2">
+                            {issuesWithMostComments.map((issue, idx) => (
+                                <Fragment key={idx}>
+                                    <IssueCard key={idx} issue={issue} />
+                                </Fragment>
+                            ))}
+                        </div>
                     </div>
-                </div>
+                    <Divider />
+                    <div className="rounded-8">
+                        <div className="text-std-20B-5 pb-4 text-center" id="latestIssues">
+                            <a
+                                href="https://github.com/takahiroanno2024/election2024/issues?q=is%3Aopen+label%3A%E7%B5%8C%E6%B8%88%2C%E5%8C%BB%E7%99%82%E3%83%BB%E9%98%B2%E7%81%BD%2C%E6%95%99%E8%82%B2%E3%83%BB%E5%AD%90%E8%82%B2%E3%81%A6%2C%E8%A1%8C%E6%94%BF%2C%E6%B0%91%E4%B8%BB%E4%B8%BB%E7%BE%A9%2C%E3%81%9D%E3%81%AE%E4%BB%96" target="_blank" rel="noopener noreferrer"
+                                className="text-primary underline">最新のIssue</a>
+                        </div>
+                        <div className="space-y-0 md:space-y-2">
+                            {latestIssues.map((issue, idx) => (
+                                <Fragment key={idx}>
+                                    <div className="flex">
 
-                <div className="rounded-8 bg-white">
-                    <div className="text-std-20B-5 p-4" id="mostReactionIssues">
-                        <a href="https://github.com/takahiroanno2024/election2024/issues?q=is%3Aissue+label%3A%E7%B5%8C%E6%B8%88%2C%E5%8C%BB%E7%99%82%E3%83%BB%E9%98%B2%E7%81%BD%2C%E6%95%99%E8%82%B2%E3%83%BB%E5%AD%90%E8%82%B2%E3%81%A6%2C%E8%A1%8C%E6%94%BF%2C%E6%B0%91%E4%B8%BB%E4%B8%BB%E7%BE%A9%2C%E3%81%9D%E3%81%AE%E4%BB%96+sort%3Areactions-%2B1-desc+"
-                            target="_blank" rel="noopener noreferrer"
-                            className="text-primary underline">
-                            👍️の多いIssue
-                        </a>
+                                        <IssueCard key={idx} issue={issue} />
+                                    </div>
+                                </Fragment>
+                            ))}
+                        </div>
                     </div>
-                    <div className="space-y-2">
-                        {issuesWithMostReactions.map((issue, idx) => (
-                            <Fragment key={idx}>
-                                <IssueCard key={idx} issue={issue} />
-                            </Fragment>
-                        ))}
-                    </div>
+                    <Divider />
                 </div>
-                <div className="rounded-8 bg-white">
-                    <div className="text-std-20B-5 p-4" id="mostCommentsIssues">
-                        <a href="https://github.com/takahiroanno2024/election2024/issues?q=is%3Aissue+label%3A%E7%B5%8C%E6%B8%88%2C%E5%8C%BB%E7%99%82%E3%83%BB%E9%98%B2%E7%81%BD%2C%E6%95%99%E8%82%B2%E3%83%BB%E5%AD%90%E8%82%B2%E3%81%A6%2C%E8%A1%8C%E6%94%BF%2C%E6%B0%91%E4%B8%BB%E4%B8%BB%E7%BE%A9%2C%E3%81%9D%E3%81%AE%E4%BB%96+sort%3Acomments-desc"
-                            target="_blank" rel="noopener noreferrer"
-                            className="text-primary underline">
-                            コメントの多いIssue
-                        </a>
-                    </div>
-                    <div className="space-y-2">
-                        {issuesWithMostComments.map((issue, idx) => (
-                            <Fragment key={idx}>
-                                <IssueCard key={idx} issue={issue} />
-                            </Fragment>
-                        ))}
-                    </div>
+                <div className="hidden md:inline-block p-4">
+                    <Anchor offsetTop={10} items={[
+                        { key: "mostReaction", href: "#mostReactionIssues", title: "👍️の多いIssue" },
+                        { key: "mostComments", href: "#mostCommentsIssues", title: "コメントの多いIssue" },
+                        { key: "latest", href: "#latestIssues", title: "最新のIssue" },
+
+                    ]} />
                 </div>
             </div>
         </div>
